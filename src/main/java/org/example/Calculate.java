ece47f7d5892c1442749924970deb46472c01cc3
nilairisusanka <nilagirisusanka@gmail.com>
2024-02-19 18:38:17 +0530

    created a class named Calculate and added a method calculate difference

---
+++ src/main/java/org/example/Calculate.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    Scanner scanner = new Scanner(System.in);
+    int a= scanner.nextInt();
+    int b== scanner.nextInt();
+    public calculate(){
+        return a-b;
+    }
+}
