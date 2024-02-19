be8e010eb522281ab5792666d3eb692df781d52b
nilairisusanka <nilagirisusanka@gmail.com>
2024-02-19 18:45:23 +0530

    created a class named Calculate and added a method calculate difference

--- src/main/java/org/example/Calculate.java
+++ src/main/java/org/example/Calculate.java
@@ -2,7 +2,7 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Calculate {
 
     public calculate(int a,int b){
         return a-b;
